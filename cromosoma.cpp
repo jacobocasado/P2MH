1845bf255c8b440fe21225a1f109e884aa171a2d
jcasado <casacojacobo@gmail.com>
2021-04-20 18:55:30 +0200

    Inicio de la practica 2 de MH. Jacobo Casado de Gracia

---
+++ cromosoma.cpp
@@ -0,0 +1,15 @@
+//
+// Created by jcasado on 20/04/2021.
+//
+
+#include "cromosoma.h"
+
+Cromosoma::Cromosoma(int size, int subsize){
+
+    int genes = 0;
+
+    for (int i = 0; i < size; ++i){
+
+    }
+
+}
---
+++ cromosoma.h
@@ -0,0 +1,24 @@
+//
+// Clase cromosoma que va a albergar, tanto la generacion aleatoria de este, como los operadores basicos
+//
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+using namespace std;
+
+#ifndef P2MH_CROMOSOMA_H
+#define P2MH_CROMOSOMA_H
+
+
+
+class Cromosoma {
+
+    vector<int> genes;
+    Cromosoma(int size, int subsize);
+
+};
+
+
+#endif //P2MH_CROMOSOMA_H
